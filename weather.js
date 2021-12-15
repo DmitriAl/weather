06ed56d40ff9ae20b3da9554d5e95af8b26a48b3
Dmitri Alexandrow <dorfstil@gmail.com>
2021-12-16 00:43:03 +0400

    make getting cli argvs

--- weather.js
+++ weather.js
@@ -1,1 +1,19 @@
 #!/user/bin/env node
+import { getArgs } from './helpers/args.js';
+
+const iniCLI = () => {
+    const args = getArgs(process.argv);
+    console.log(args);
+    if (args.h) {
+        // vivod help
+    }
+    if (args.s) {
+        // save city
+    }
+    if (args.t) {
+        // save token
+    }
+    // vivesti pogodu
+};
+
+iniCLI();
