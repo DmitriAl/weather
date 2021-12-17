56e0711508d3c3ffe96b135f4d94f74a4e7178dd
Dmitri Alexandrow <dorfstil@gmail.com>
2021-12-17 16:03:04 +0400

    refactor save token func

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 #!/user/bin/env node
 import { getArgs } from './helpers/args.js';
 import { printHelp, printSuccess, printError } from "./services/log.service.js";
-import {saveKeyValue} from "./services/storage.service.js";
+import {saveKeyValue, TOKEN_DICTIONARY} from "./services/storage.service.js";
 
 const saveToken = async (token) => {
     if (!token.length) {
@@ -9,7 +9,7 @@ const saveToken = async (token) => {
         return;
     }
     try {
-        await saveKeyValue('token', token);
+        await saveKeyValue(TOKEN_DICTIONARY.token, token);
         printSuccess('Токен сохранен');
     } catch (error) {
         printError(error.message);
