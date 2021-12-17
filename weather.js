78f27f13f8b6c4c9308fe9e121e374d609385650
Dmitri Alexandrow <dorfstil@gmail.com>
2021-12-17 16:44:58 +0400

    add api service

--- weather.js
+++ weather.js
@@ -1,7 +1,8 @@
 #!/user/bin/env node
-import { getArgs } from './helpers/args.js';
-import { printHelp, printSuccess, printError } from "./services/log.service.js";
+import {getArgs} from './helpers/args.js';
+import {printHelp, printSuccess, printError} from "./services/log.service.js";
 import {saveKeyValue, TOKEN_DICTIONARY} from "./services/storage.service.js";
+import {getWeather} from "./services/api.service.js";
 
 const saveToken = async (token) => {
     if (!token.length) {
